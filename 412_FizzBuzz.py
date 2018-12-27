a344621a3b43f9bf1fed55f16e594877e0183d6b
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-12-28 00:08:00 +0530

    Python Solution

--- 412_FizzBuzz.py
+++ 412_FizzBuzz.py
@@ -1,11 +1,8 @@
 """
 Write a program that outputs the string representation of numbers from 1 to n.
 But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
-
 Example:
-
 n = 15,
-
 Return:
 [
     "1",
