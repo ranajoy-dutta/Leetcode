0485de38bb37f9a2a3bae1809bb1ad61902a33e2
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-12-28 22:36:31 +0530

    Issue Resolved

--- 001_Two-Sum.py
+++ 001_Two-Sum.py
@@ -12,10 +12,10 @@ return [0, 1].
 """
 
 class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-        
-        
+    def twosum(self, nums, target):
+        mydict = {}
+        for i, num in enumerate(nums):
+            if (target-num) in mydict:
+                return mydict[target-num], i
+            mydict[num] = i   
+            
