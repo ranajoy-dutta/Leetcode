20dac4b10ad344aaf6faebea489092bf9778ec5d
Ranajoy Dutta <ranajoydutta7@gmail.com>
2019-01-03 23:37:29 +0530

    Solution In Python

--- 709_to_lowercase.py
+++
@@ -1,7 +0,0 @@
-"""
-Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
-"""
-class Solution:
-    def toLowerCase(self, str):
-        return ''.join([chr(ord('a')+ord(s)-ord('A')) if ord(s) <= ord('Z') and ord(s) >= ord('A') else s for s in str])
-        
